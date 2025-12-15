18d041d3b2a95f0444db20a0aa5cf792d1890562
Shyam Maurya <105080@EOSINDIA.COM>
2025-12-15 16:29:48 +0530

    Updated REST_FRAMEWORK settings and authentication config

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -50,4 +50,12 @@ STATIC_URL='static/'
 DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 AUTH_USER_MODEL='accounts.User'
 
-REST_FRAMEWORK={'DEFAULT_AUTHENTICATION_CLASSES':[]}
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated"
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
+    ]
+}
+
